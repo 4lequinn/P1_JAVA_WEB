25bd7b376af3624690527fa8dee3fc1a44bcc0ee
4lequinn <jorgealequinn@gmail.com>
2021-09-15 19:27:42 -0300

    PRUEBA

---
+++ src/java/modelo/dao/UsuarioDAO.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modelo.dao;
+
+import modelo.crud.MetodosModelo;
+import modelo.dto.Usuario;
+
+/**
+ * 
+ * @author Jorge
+ */
+
+public class UsuarioDAO extends MetodosModelo<Usuario>{
+
+    @Override
+    public String getNombreModelo() {
+        return "Usuario";
+    }
+    
+    // Desarrollar los métodos
+    
+    // Login
+}
